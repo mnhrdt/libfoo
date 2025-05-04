a28c06a0041d9b3e492c50022fcaaa340e99883e
enric meinhardt-llopis <enric.meinhardt@ens-paris-saclay.fr>
2025-05-05 01:11:57 +0200

    add readme

--- with_setuptools/setup.py
+++ with_setuptools/setup.py
@@ -5,12 +5,12 @@ class my_build(build_py):
 		from os import system as sh
 		sh("cc -O3 -march=native -shared -fPIC foo.c -o libfoo.so")
 		super().run()
-		sh(f"libfoo.so {self.build_lib}/libfoo.so")
+		sh(f"cp libfoo.so {self.build_lib}/libfoo.so")
 
 from setuptools import setup
 setup(
-	name='foo',
-	version='1',
-	py_modules=['foo'],
+	name="foo",
+	version="1",
+	py_modules=["foo"],
 	cmdclass={'build_py': my_build},
 )
