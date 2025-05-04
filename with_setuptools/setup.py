e7754393e7c852a8816a76f675a423d9e7340570
enric meinhardt-llopis <enric.meinhardt@ens-paris-saclay.fr>
2025-05-05 00:23:30 +0200

    simplify setup.py

--- with_setuptools/setup.py
+++ with_setuptools/setup.py
@@ -1,21 +1,16 @@
-from setuptools import setup
+
 from setuptools.command.build_py import build_py
-import subprocess
-import shutil
-
-class BuildWithCLib(build_py):
+class my_build(build_py):
 	def run(self):
-		subprocess.check_call(['gcc', '-shared', '-o', 'libfoo.so', '-fPIC', 'foo.c'])
+		from os import system as sh
+		sh("cc -O3 -march=native -shared -fPIC foo.c -o libfoo.so")
 		super().run()
-		build_lib_path = self.build_lib
-		target_path = os.path.join(build_lib_path, 'libfoo.so')
-		shutil.copyfile('libfoo.so', target_path)
+		sh(f"libfoo.so {self.build_lib}/libfoo.so")
 
+from setuptools import setup
 setup(
 	name='foo',
-	version='0.1',
+	version='1',
 	py_modules=['foo'],
-	package_data={'': ['libfoo.so']},
-	include_package_data=True,
-	cmdclass={'build_py': BuildWithCLib},
+	cmdclass={'build_py': my_build},
 )
